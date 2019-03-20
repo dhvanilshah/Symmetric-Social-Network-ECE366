2196c29b6b397d34ceeaab78ebb031557f582b9c
Ariana Freitag <freitagariana@gmail.com>
2019-03-19 22:29:40 -0400

    login component init

---
+++ my-app/src/components/register.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react'
+
+class Register extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      first_name:'',
+      last_name:'',
+      email:'',
+      username:'',
+      password:'',
+      service:''
+    }
+  }
+  class Register extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      first_name:'',
+      last_name:'',
+      email:'',
+      password:''
+    }
+  }
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Register"
+           />
+           <TextField
+             hintText="Enter your First Name"
+             floatingLabelText="First Name"
+             onChange = {(event,newValue) => this.setState({first_name:newValue})}
+             />
+           <br/>
+           <TextField
+             hintText="Enter your Last Name"
+             floatingLabelText="Last Name"
+             onChange = {(event,newValue) => this.setState({last_name:newValue})}
+             />
+           <br/>
+           <TextField
+             hintText="Enter your Email"
+             type="email"
+             floatingLabelText="Email"
+             onChange = {(event,newValue) => this.setState({email:newValue})}
+             />
+           <br/>
+           <TextField
+             type = "password"
+             hintText="Enter your Password"
+             floatingLabelText="Password"
+             onChange = {(event,newValue) => this.setState({password:newValue})}
+             />
+           <br/>
+           <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+          </div>
+         </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+const style = {
+  margin: 15,
+};
+export default Register;
---
+++ my-app/src/containers/loginscreen.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import Login from '../components/Login';
+import Register from '../containers/Register';
+
+const style = {
+  margin: 15,
+};
+
+class Loginscreen extends Component {
+  constructor(props){
+    super(props);
+    var loginButtons=[];
+    loginButtons.push(
+      <div>
+      <MuiThemeProvider>
+        <div>
+           <RaisedButton label={"Register as Student"} primary={true} style={style} onClick={(event) => this.handleClick(event,'student')}/>
+       </div>
+       </MuiThemeProvider>
+       <MuiThemeProvider>
+       <div>
+          <RaisedButton label={"Register as Teacher"} primary={true} style={style} onClick={(event) => this.handleClick(event,'teacher')}/>
+      </div>
+      </MuiThemeProvider>
+      </div>
+    )
+    this.state={
+      username:'',
+      password:'',
+      loginscreen:[],
+      loginmessage:'',
+      loginButtons:loginButtons,
+      registerButtonLabel:registerButtons,,
+      isLogin:true
+    }
+  }
+  componentWillMount(){
+    var loginscreen=[];
+    loginscreen.push(<Login parentContext={this} appContext={this.props.appContext}/>);
+    var loginmessage = "Not registered yet, Register Now";
+    this.setState({
+                  loginscreen:loginscreen,
+                  loginmessage:loginmessage
+                    })
+  }
+  handleClick(event,userRole){
+    console.log("event",userRole);
+    var loginmessage;
+    if(this.state.isLogin){
+      let loginscreen=[];
+      loginscreen.push(<Register parentContext={this} appContext={this.props.appContext} role={userRole}/>);
+      loginmessage = "Already registered. Please login";
+      let loginButtons=[];
+      loginButtons.push(
+        <div key="login-button">
+        <MuiThemeProvider>
+          <div>
+             <RaisedButton label={"Login"} primary={true} style={style} onClick={(event) => this.handleClick(event,userRole)}/>
+         </div>
+         </MuiThemeProvider>
+        </div>
+      )
+      this.setState({
+                     loginscreen:loginscreen,
+                     loginmessage:loginmessage,
+                     loginButtons:loginButtons,
+                     isLogin:false
+                   })
+    }
+    else{
+      let loginscreen=[],loginButtons=[];
+      loginButtons.push(
+        <div>
+        <MuiThemeProvider>
+          <div>
+             <RaisedButton label={"Register as Student"} primary={true} style={style} onClick={(event) => this.handleClick(event,'student')}/>
+         </div>
+         </MuiThemeProvider>
+        </div>
+      )
+      loginscreen.push(<Login parentContext={this} appContext={this.props.appContext} role={userRole}/>);
+      loginmessage = "Not Registered yet.Go to registration";
+      this.setState({
+                     loginscreen:loginscreen,
+                     loginmessage:loginmessage,
+                     loginButtons:loginButtons,
+                     isLogin:true
+                   })
+    }
+  }
+  render() {
+    return (
+      <div className="loginscreen" key="loginscreen">
+        {this.state.loginscreen}
+        <div>
+          {this.state.loginmessage}
+          {this.state.loginButtons}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Loginscreen;
